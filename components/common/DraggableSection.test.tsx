2408f478fea6d6bd9bc5c354275af3f419162d97
Dan Le <dale@axon.com>
2026-01-02 11:22:34 +0700

    Clean up: Remove unused ESLint disable directive

--- components/common/DraggableSection.test.tsx
+++ components/common/DraggableSection.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
